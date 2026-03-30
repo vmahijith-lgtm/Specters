68560d3df863ab55e83c1ffd66a0ac74a3ce68c2
vmahijith-lgtm <vmahijith@gmail.com>
2026-03-31 01:26:14 +0530

    chore: clean up unwanted test scripts

--- apps/web/test_await.js
+++
@@ -1,5 +0,0 @@
-async function foo() {
-  const p = Promise.resolve({});
-  console.log(p.auth.getUser());
-}
-foo().catch(e => console.log(e.message));
--- apps/web/test_supabase.ts
+++
@@ -1,2 +0,0 @@
-import { createServerClient } from '@supabase/ssr'
-console.log(createServerClient('http://localhost', 'anon', { cookies: { getAll: () => [], setAll: () => {} } }).auth)
