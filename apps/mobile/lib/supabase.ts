156f272ce4207176405ef30c17fb3a69877ebe81
vmahijith-lgtm <vmahijith@gmail.com>
2026-04-02 22:00:30 +0530

    chore: remove files now tracked in gitignore

--- apps/mobile/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import Constants from 'expo-constants'
-
-const SUPABASE_URL = Constants.expoConfig?.extra?.supabaseUrl as string
-const SUPABASE_KEY = Constants.expoConfig?.extra?.supabaseAnonKey as string
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
